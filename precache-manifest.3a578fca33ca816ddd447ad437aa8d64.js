993d079e6c2e95e26d829379863476656bbf27bc
Rushikesh V <www.rushikeshvidhate02@gmail.com>
2019-12-31 12:26:23 +0530

    Updates

---
+++ precache-manifest.3a578fca33ca816ddd447ad437aa8d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d5ea24dafa62d6b858b1e6b2d0d362",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "877e85f413e68c9e2970",
+    "url": "/monsters-rolodex/static/css/main.720a1b26.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "877e85f413e68c9e2970",
+    "url": "/monsters-rolodex/static/js/main.1bb9af55.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
